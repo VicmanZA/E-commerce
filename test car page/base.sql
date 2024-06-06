5d4cbe1ee17bbeaaa0994889c0427db3cea005e0
VicmanZA <101719633+VicmanZA@users.noreply.github.com>
2024-06-06 15:08:49 +0200

    Add files via upload

---
+++ test car page/base.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE my_database;
+
+USE my_database;
+
+CREATE TABLE your_table (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(50)
+);
